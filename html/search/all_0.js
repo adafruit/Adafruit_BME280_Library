c2d61baa979f7633b397799d9a852c2244167e72
Travis CI <travis@travis-ci.org>
2019-02-04 05:25:06 +0000

    Deploy code docs to GitHub Pages Travis build: 7
    Commit: 24a162b0bb69577ad5dd5f1e81352ca3640344dd

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,6 +3,5 @@ var searchData=
   ['adafruit_5fbme280',['Adafruit_BME280',['../class_adafruit___b_m_e280.html',1,'Adafruit_BME280'],['../class_adafruit___b_m_e280.html#a67b78a7dd732fab0b34372235945824f',1,'Adafruit_BME280::Adafruit_BME280(void)'],['../class_adafruit___b_m_e280.html#aeafbace9aef944d2e82745cf1ec5a85f',1,'Adafruit_BME280::Adafruit_BME280(int8_t cspin)'],['../class_adafruit___b_m_e280.html#a60037360f9b92a42fef0959f493a9407',1,'Adafruit_BME280::Adafruit_BME280(int8_t cspin, int8_t mosipin, int8_t misopin, int8_t sckpin)']]],
   ['adafruit_5fbme280_2ecpp',['Adafruit_BME280.cpp',['../_adafruit___b_m_e280_8cpp.html',1,'']]],
   ['adafruit_5fbme280_2eh',['Adafruit_BME280.h',['../_adafruit___b_m_e280_8h.html',1,'']]],
-  ['adafruit_20bme280_20humidity_2c_20temperature_20_26_20pressure_20sensor',['Adafruit BME280 humidity, temperature &amp; pressure sensor',['../index.html',1,'']]],
-  ['adafruit_20bme280_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fbme280_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fbme280_5flibrary_29',['Adafruit BME280 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_BME280_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_BME280_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20bme280_20humidity_2c_20temperature_20_26_20pressure_20sensor',['Adafruit BME280 humidity, temperature &amp; pressure sensor',['../index.html',1,'']]]
 ];
