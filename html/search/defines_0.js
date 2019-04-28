e32b9f1649b2d5c03c2dadab8639627d8ec51dde
Travis CI <travis@travis-ci.org>
2019-04-28 11:46:24 +0000

    Deploy code docs to GitHub Pages Travis build: 25
    Commit: 1f2d47a6fe599b01464ea608386ae9ea9f985306

--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bme280_5faddress',['BME280_ADDRESS',['../_adafruit___b_m_e280_8h.html#a632fe389011b56233474fad5bc075e4b',1,'Adafruit_BME280.h']]]
+  ['bme280_5faddress',['BME280_ADDRESS',['../_adafruit___b_m_e280_8h.html#a632fe389011b56233474fad5bc075e4b',1,'Adafruit_BME280.h']]],
+  ['bme280_5faddress_5falternate',['BME280_ADDRESS_ALTERNATE',['../_adafruit___b_m_e280_8h.html#abe75d276a7fcf8ff7c0c7dc6fb20d5e9',1,'Adafruit_BME280.h']]]
 ];
