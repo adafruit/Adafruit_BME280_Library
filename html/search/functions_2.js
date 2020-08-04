9dafe23961c5292664cff38e6a1c889024b8acf7
Doxygen CI <travis@travis-ci.org>
2020-08-04 13:49:53 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -5,5 +5,6 @@ var searchData=
   ['gethumiditysensor',['getHumiditySensor',['../class_adafruit___b_m_e280.html#ae1c90512a947f8b953b51253d023551d',1,'Adafruit_BME280']]],
   ['getpressuresensor',['getPressureSensor',['../class_adafruit___b_m_e280.html#a181ade838c305a2a27b999b76017b7ee',1,'Adafruit_BME280']]],
   ['getsensor',['getSensor',['../class_adafruit___b_m_e280___temp.html#ab5b5fb254bc4d0f3b6bf9e9db867b1a0',1,'Adafruit_BME280_Temp::getSensor()'],['../class_adafruit___b_m_e280___pressure.html#aef83fa8b2b4b7a321d2e117d02b3cdc0',1,'Adafruit_BME280_Pressure::getSensor()'],['../class_adafruit___b_m_e280___humidity.html#a5cc2ae7b5d1b67857665c161f17955e4',1,'Adafruit_BME280_Humidity::getSensor()']]],
+  ['gettemperaturecompensation',['getTemperatureCompensation',['../class_adafruit___b_m_e280.html#acea468fd952aaf1354efe75c1ef61fe5',1,'Adafruit_BME280']]],
   ['gettemperaturesensor',['getTemperatureSensor',['../class_adafruit___b_m_e280.html#ac7012b53a26f3486219e75cd704c01ab',1,'Adafruit_BME280']]]
 ];
