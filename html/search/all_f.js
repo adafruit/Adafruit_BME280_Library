61856fdc1d82db7d86e12f6b25776f3263966359
Doxygen CI <travis@travis-ci.org>
2020-10-31 00:11:10 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -3,6 +3,6 @@ var searchData=
   ['t_5ffine',['t_fine',['../class_adafruit___b_m_e280.html#a8d29a42e8046d1958a5d729571bea8d0',1,'Adafruit_BME280']]],
   ['t_5ffine_5fadjust',['t_fine_adjust',['../class_adafruit___b_m_e280.html#a04b6f4d267aa2deb9f6ae393d53815f6',1,'Adafruit_BME280']]],
   ['t_5fsb',['t_sb',['../struct_adafruit___b_m_e280_1_1config.html#aae0bbe25ab1baec8a473a9a6b5d65d66',1,'Adafruit_BME280::config']]],
-  ['takeforcedmeasurement',['takeForcedMeasurement',['../class_adafruit___b_m_e280.html#ab2e33adc3154e5ff422bea32b324dba0',1,'Adafruit_BME280']]],
+  ['takeforcedmeasurement',['takeForcedMeasurement',['../class_adafruit___b_m_e280.html#a4c4bbbcab833ce1a08d8de2d6acea308',1,'Adafruit_BME280']]],
   ['temp_5fsensor',['temp_sensor',['../class_adafruit___b_m_e280.html#a9ef0aa2e70fc9afb3628ad8ca6a2796d',1,'Adafruit_BME280']]]
 ];
--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['takeforcedmeasurement',['takeForcedMeasurement',['../class_adafruit___b_m_e280.html#ab2e33adc3154e5ff422bea32b324dba0',1,'Adafruit_BME280']]]
+  ['takeforcedmeasurement',['takeForcedMeasurement',['../class_adafruit___b_m_e280.html#a4c4bbbcab833ce1a08d8de2d6acea308',1,'Adafruit_BME280']]]
 ];
